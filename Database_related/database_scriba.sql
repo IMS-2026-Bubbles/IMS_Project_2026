f1689e54c5e497caa2886bd0de9019ed5ef58cfb
COEN5007 <cornelia.engman.5007@student.uu.se>
2026-09-22 11:13:34 +0200

    check database_scriba_lucid.sql
    it should work

--- Database_related/database_scriba.sql
+++ Database_related/database_scriba.sql
@@ -1,9 +1,94 @@
 
 
-/**/ database sql code start
+/*database sql code start*/ 
+/* parent table n*/ 
+/* this is not the final porduct*/
 
-CREATE TABLE company (
-  Company_ID datatype VARCHAR(10),
-  Company_Name datatype VARCHAR(100),
+CREATE TABLE Company (
+    Company_ID VARCHAR(10) PRIMARY KEY /*AUTO_INCREMENT*/, /*till company memeber*/ 
+    Comp_Name VARCHAR(100)
 );
+
+/*both parent and child table? */ 
+
+CREATE TABLE Lab_Group (
+    Lab_Group_ID VARCHAR(10) PRIMARY KEY /*AUTO_INCREMENT*/,/* LAB group memeber till ID*/ 
+    Comapny_ID VARCHAR(10) FOREIGN KEY, /* company memeber längst upp och company ID) */
+    Lab_Name VARCHAR(100)
+);
+
+CREATE TABLE Company_Member(
+    Company_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY /*AUTO_INCREMENT*/, /* linked to company (compny id) and fk company id inn lab group*/ 
+    User_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY,
+    Role ENUM('admin', 'member') NOT NULL
+);
+
+
+CREATE TABLE Lab_Group_Member(
+    Lab_Group_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY /*AUTO_INCREMENT*/, /*lab group linked*/
+    User_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY, /* linked to both user and lab group*/
+    Role ENUM('admin', 'member') NOT NULL
+);
+
+
+CREATE TABLE USER(
+    User_ID VARCHAR(10) PRIMARY KEY /*AUTO_INCREMENT*/, /* To lab group memeber and company memebr */
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Salt VARCHAR(255),
+    
+    Changes_saved INT,
+    Last_Login TIMESTAMP,
+    Streak INT
+    
+);
+
+
+CREATE TABLE Proj_Tag(
+    Project_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY,
+    Proj_Tag VARCHAR(10)
+);
+
+
+CREATE TABLE Project(
+    Project_ID VARCHAR(10) PRIMARY KEY, 
+    Project_Name VARCHAR(255) NOT NULL,
+    Lab_Group_ID VARCHAR(10) NOT NULL FOREIGN KEY, /* FOREIGN KEY TO Lab_group*/
+    Date_Created TIMESTAMP,
+    Project_Done BOOLEAN,
+);
+
+CREATE TABLE Project_Member(
+    Project_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY, /* länkad till project tag */
+    User_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY,
+    Role ENUM('owner', 'edit', 'read') NOT NULL
+);
+
+CREATE TABLE Exp_Tag(
+    Experiment_ID VARCHAR(10) PRIMARY KEY FOREIGN KEY,
+    Exp_Tag VARCHAR(10) PRIMARY KEY, 
+);
+
+CREATE TABLE Proj_Experiment(
+    Experiment_ID VARCHAR(10) PRIMARY KEY,
+    Experiment_Name VARCHAR(255) NOT NULL,
+    Project_ID VARCHAR(10) FOREIGN KEY, /* till project*/
+    Date_Created TIMESTAMP,
+    Date_Updated TIMESTAMP,
+    Plan_Text TEXT,
+    Plan_Updated TIMESTAMP,
+    Plan_Done BOOLEAN Log_Text TEXT,
+    Log_Updated TIMESTAMP,
+    Log_Done BOOLEAN,
+    Result_Text TEXT,
+    Result_Updated TIMESTAMP,
+    Result_Done BOOLEAN
+);
+
+
+
+
+
+
 
---
+++ Database_related/database_scriba_lucid.sql
@@ -0,0 +1,121 @@
+
+CREATE TABLE `Company` (
+  `Company_ID` VARCHAR(10),
+  `Comp_Name` VARCHAR(100),
+  PRIMARY KEY (`Company_ID`)
+);
+
+CREATE TABLE `Lab_Group` (
+  `Lab_Group_ID` VARCHAR(10),
+  `Company_ID` VARCHAR(10),
+  `Lab_Name` VARCHAR(100),
+  PRIMARY KEY (`Lab_Group_ID`),
+  FOREIGN KEY (`Company_ID`)
+      REFERENCES `Company`(`Company_ID`)
+);
+
+CREATE TABLE `Project` (
+  `Project_ID` VARCHAR(10),
+  `Project_Name` VARCHAR(255) NOT NULL,
+  `Lab_Group_ID` VARCHAR(10) NOT NULL,
+  `Date_Created` TIMESTAMP,
+  `Date_Updated` TIMESTAMP,
+  `Project_Done` BOOLEAN,
+  PRIMARY KEY (`Project_ID`),
+  FOREIGN KEY (`Lab_Group_ID`)
+      REFERENCES `Lab_Group`(`Lab_Group_ID`)
+);
+
+CREATE TABLE `Proj_Tag` (
+  `Project_ID` VARCHAR(10),
+  `Proj_Tag` VARCHAR(10),
+  PRIMARY KEY (`Project_ID`, `Proj_Tag`),
+  FOREIGN KEY (`Project_ID`)
+      REFERENCES `Project`(`Project_ID`)
+);
+
+CREATE TABLE `Proj_Experiment` (
+  `Experiment_ID` VARCHAR(10),
+  `Experiment_Name` VARCHAR(255) NOT NULL,
+  `Project_ID` VARCHAR(10),
+  `Date_Created` TIMESTAMP,
+  `Date_Updated` TIMESTAMP,
+  `Plan_Text` TEXT,
+  `Plan_Updated` TIMESTAMP,
+  `Plan_Done` BOOLEAN,
+  `Log_Text` TEXT,
+  `Log_Updated` TIMESTAMP,
+  `Log_Done` BOOLEAN,
+  `Result_Text` TEXT,
+  `Result_Updated` TIMESTAMP,
+  `Result_Done` BOOLEAN,
+  PRIMARY KEY (`Experiment_ID`),
+  FOREIGN KEY (`Project_ID`)
+      REFERENCES `Project`(`Project_ID`)
+);
+
+CREATE TABLE `Exp_Tag` (
+  `Experiment_ID` VARCHAR(10),
+  `Exp_Tag` VARCHAR(10),
+  PRIMARY KEY (`Experiment_ID`, `Exp_Tag`),
+  FOREIGN KEY (`Experiment_ID`)
+      REFERENCES `Proj_Experiment`(`Experiment_ID`)
+);
+
+CREATE TABLE `User` (
+  `User_ID` VARCHAR(10),
+  `Email` VARCHAR(255) NOT NULL UNIQUE,
+  `First_Name` VARCHAR(50),
+  `Last_Name` VARCHAR(50),
+  `Salt` BINARY(16),
+  `Password` VARCHAR(255),
+  `Changes_Saved` INT,
+  `Last_Login` TIMESTAMP,
+  `Streak` INT,
+  PRIMARY KEY (`User_ID`)
+);
+
+CREATE TABLE `Company_Member` (
+  `Company_ID` VARCHAR(10),
+  `User_ID` VARCHAR(10),
+  `Role` ENUM('admin', 'member') NOT NULL,
+  PRIMARY KEY (`Company_ID`, `User_ID`),
+  FOREIGN KEY (`User_ID`)
+      REFERENCES `User`(`User_ID`),
+  FOREIGN KEY (`Company_ID`)
+      REFERENCES `Company`(`Company_ID`)
+);
+
+CREATE TABLE `Project_Member` (
+  `Project_ID` VARCHAR(10),
+  `User_ID` VARCHAR(10),
+  `Role` ENUM('owner', 'edit', 'read') NOT NULL,
+  PRIMARY KEY (`Project_ID`, `User_ID`),
+  FOREIGN KEY (`Project_ID`)
+      REFERENCES `Project`(`Project_ID`),
+  FOREIGN KEY (`User_ID`)
+      REFERENCES `User`(`User_ID`)
+);
+
+CREATE TABLE `Experiment_Member` (
+  `Experiment_ID` VARCHAR(10),
+  `User_ID` VARCHAR(10),
+  `Role` ENUM('edit', 'read') NOT NULL,
+  PRIMARY KEY (`Experiment_ID`, `User_ID`),
+  FOREIGN KEY (`User_ID`)
+      REFERENCES `User`(`User_ID`),
+  FOREIGN KEY (`Experiment_ID`)
+      REFERENCES `Proj_Experiment`(`Experiment_ID`)
+);
+
+CREATE TABLE `Lab_Group_Member` (
+  `Lab_Group_ID` VARCHAR(10),
+  `User_ID` VARCHAR(10),
+  `Role` ENUM('admin', 'member') NOT NULL,
+  PRIMARY KEY (`Lab_Group_ID`, `User_ID`),
+  FOREIGN KEY (`Lab_Group_ID`)
+      REFERENCES `Lab_Group`(`Lab_Group_ID`),
+  FOREIGN KEY (`User_ID`)
+      REFERENCES `User`(`User_ID`)
+);
+
