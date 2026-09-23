88f6fe7309f03c51f59db58145d7a9e9cbaa782b
tildawiklund <tildawiklund2002@gmail.com>
2026-09-23 10:19:41 +0200

    for meeting

--- scriba_admin.php
+++ scriba_admin.php
@@ -1,3 +1,55 @@
+<?php
+    include 'Database_related/db.php';
+
+    # if button to register new:
+    if(isset($_POST['register_company']))
+    {
+    # fetch data from POST request
+    $company_name = $_POST['company_name'];
+
+    # use placeholders to protect against sql injection
+    $sql = "INSERT INTO Company(Company_Name) VALUES (?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $company_name);
+    $result = $stmt->execute();
+
+    # echos how it went
+    if ($result) {
+        echo "New company registered to Scriba";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    }
+
+    # add a check here as well so that the same company isn't added twice
+
+
+
+    // OBS i don't know if this works
+    // the idea is that all info regarding a user will be saved?
+    $sql = "SELECT User.User_ID, User.First_Name, User.Last_Name, User.Email, Company.Comp_Name, Lab_Group.Lab_Name, Lab_Group_Member.Lab_Group_ID
+            FROM User
+            LEFT JOIN Company_Member ON User.User_ID = Company_Member.User_ID
+            LEFT JOIN Company ON Company_Member.Company_ID = Company.Company_ID
+            LEFT JOIN Lab_Group_Member ON User.User_ID = Lab_Group_Member.User_ID
+            LEFT JOIN Lab_Group ON Lab_Group_Member.Lab_Group_ID = Lab_Group.Lab_Group_ID";
+    
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $rows_to_display .= "<tr> <td>" . $row["First_Name"] . " " . $row["Last_Name"] .
+                "</td><td>" . $row["Comp_Name"] .  "</td></tr>";
+        }
+    } else {
+        $rows_to_display = "No members of Scriba.";
+    }
+
+    
+    // disconnect from database
+    include "database_related/closeDB.php";
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,40 +80,6 @@
     <input type="submit" class="btn btn-dark rounded-pill" name="register_company" value="Register"><br><br>
     </form>
 
-
-
-<?php
-    include 'Database_related/db.php';
-
-    # if button to register new:
-    if(isset($_POST['register_company']))
-    {
-    # fetch data from POST request
-    $company_name = $_POST['company_name'];
-
-    # use placeholders to protect against sql injection
-    $sql = "INSERT INTO Company(Company_Name) VALUES (?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $company_name);
-    $result = $stmt->execute();
-
-    # echos how it went
-    if ($result) {
-        echo "New company registered to Scriba";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-    }
-
-    # add a check here as well so that the same company isn't added twice
-
-    include 'Database_related/closeDB.php';
-?>
-
-
-
-
-
     <table class = "table table-striped"> <!-- update to another class maybe -->
         <thead>
         <tr>
@@ -70,42 +88,10 @@
         <th scope="col">Company</th>  <!-- Come up with a way for connecting to company table -->
         </tr>
     <thead> 
+    <tbody>
+        <?php echo $rows_to_display; ?>
+    </tbody>
     
-    <?php
-    // connect to database
-    include "database_related/db.php";
-
-
-
-    // OBS i don't know if this works
-    // the idea is that all info regarding a user will be saved?
-    $sql = "SELECT User.User_ID, User.First_Name, User.Last_Name, User.Email, Company.Comp_Name, Lab_Group.Lab_Name, Lab_Group_Member.Lab_Group_ID
-            FROM User
-            JOIN Company_Member ON User.User_ID = Company_Member.User_ID
-            JOIN Company ON Company_Member.Company_ID = Company.Company_ID
-            LEFT JOIN Lab_Group_Member ON User.User_ID = Lab_Group_Member.User_ID
-            LEFT JOIN Lab_Group ON Lab_Group_Member.Lab_Group_ID = Lab_Group.Lab_Group_ID";
-    
-    $stmt = $link->prepare($sql);
-    // s for string
-    $stmt->bind_param("s", $admin_ID);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "<tr> <td>" . $row["First_Name"] . " " . $row["Last_Name"] .
-                "</td><td>" . $row["Comp_Name"] .  "</td></tr>";
-        }
-    } else {
-        echo "No members of Scriba.";
-    }
-
-    
-    // disconnect from database
-    include "database_related/closeDB.php";
-    ?>
     </table>
 
     
